c153750e24124b68b634fc7fea4d763e8bc31ecc
Dru Ibarra <Druwerd@gmail.com>
2011-09-11 01:52:17 -0700

    removed space

--- dbconfig.rb
+++ dbconfig.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'data_mapper'
-require 'logger'
 ### DB SETUP ###
 
 ROOT = File.expand_path(File.dirname(__FILE__))
-DB_PATH = File.join (ROOT, 'db/scroller.db')
+DB_PATH = File.join(ROOT, 'db/scroller.db')
 
 DataMapper.setup(:default, "sqlite3://#{DB_PATH}")

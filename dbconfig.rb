5f1a2746e36a91078fdc9938f74fa1646ef05db6
Dru Ibarra <Druwerd@gmail.com>
2011-09-11 01:02:57 -0700

    removed hard coded db path

--- dbconfig.rb
+++ dbconfig.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 require 'logger'
 ### DB SETUP ###
 
-DB_PATH = '/Users/drew/Downloads/party-scroller/db/scroller.db'
+ROOT = File.expand_path(File.dirname(__FILE__))
+DB_PATH = File.join (ROOT, 'db/scroller.db')
 
 DataMapper.setup(:default, "sqlite3://#{DB_PATH}")
